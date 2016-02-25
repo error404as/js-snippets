047b3dd4190f39efcefd3287d6146a61f0443a3d
Vitali Krasnou <vitali_krasnou@epam.com>
2016-02-25 15:01:34 +0300

    js-css-inDOM
    Finds all JS/CSS files included to the page (script/link tags in DOM).

---
+++ js-css-in-dom-to-console.js
@@ -0,0 +1,35 @@
+(function(){
+/*
+Finds all JS/CSS files included to the page (script/link tags in DOM).
+Output in console.
+May be added in Developer Panel to Sources -> Snippets
+ */
+
+function getScripts(){
+	var dom = document.getElementsByTagName('script'),
+		list = [];
+	for(var i = 0, len = dom.length; i < len; i++){
+		if(dom[i].src && dom[i].src.indexOf('chrome-extension') !== 0){
+			list.push(dom[i].src);
+		}
+	}
+	return list;
+}
+function getCSSs(){
+	var dom = document.getElementsByTagName('link'),
+		list = [];
+	for(var i = 0, len = dom.length; i < len; i++){
+		if( dom[i].href && (dom[i].href.indexOf('.css') !== -1 || dom[i].getAttribute('rel') === 'stylesheet' || dom[i].getAttribute('type') === 'text/css')  ){
+			list.push(dom[i].href);
+		}
+	}
+	return list;
+}
+
+console.log ('%c%s','color: green; font: 1.2rem/1 Tahoma;', 'JS files:');
+console.log(getScripts().join('\n'));
+console.log ('%c%s','color: green; font: 1.2rem/1 Tahoma;', 'CSS files:');
+console.log(getCSSs().join('\n'));
+
+
+})();
